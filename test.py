304d753e3ecbe95cf96a2efb4aa14082a43f0691
royjafari <roh.jafari@gmail.com>
2025-01-02 11:06:47 -0800

    adds test 1 and test 2 to test.py

--- test.py
+++ test.py
@@ -1,65 +1,100 @@
 import time
-
-time.sleep(5)
-
 import numpy as np
 
 from sklearn.metrics import precision_score, recall_score, roc_auc_score, accuracy_score
-
-X = np.random.random([10000,10])
-y = np.random.choice([0,1],10000)
-c = np.random.random([10000,2])
-
-
 from soed import SOEDClassifier
-
 from sklearn.datasets import load_iris
 
-# Load dataset
-data = load_iris()
-X = data.data
-y = data.target  # Binary target
+try:
 
-y = np.where(y==2,0,y)
+    print('starting test 1.')
 
-X_standard = (X-X.min(axis=0))/(X.max(axis=0) - X.min(axis=0))
+    X = np.random.random([10000,10])
+    y = np.random.choice([0,1],10000)
+    c = np.random.random([10000,2])
 
-random_index = np.random.permutation(X.shape[0])
-i = int(round(X.shape[0]*0.5))
-train_index = random_index[:i]
-test_index = random_index[i+1:]
+    soed = SOEDClassifier(som_input_len=X.shape[1])
 
-X_train = X_standard[train_index]
-X_test = X_standard[test_index]
+    soed.fit(X,y,c)
 
-y_train = y[train_index]
-y_test = y[test_index]
+    y_proba = soed.predict_proba(X)
+    y_pred = soed.predict(X)
 
-
-c0 = np.where(y_train==0,0,20)
-c1 = np.where(y_train==1,0,1)
-
-c = np.column_stack((c0,c1))
+    y_util = soed.predict_util(X)
+    y_decide = soed.decide(X)
 
 
 
-soed = SOEDClassifier(mlp_max_iter=10000,som_x=10, som_y=10,som_input_len=X_train.shape[1])
+    recall = recall_score(y,y_pred)
+    precision = precision_score(y,y_pred)
+    accuracy = accuracy_score(y,y_pred)
+    auc = roc_auc_score(y,y_proba[:,1])
 
-soed.fit(X_train,y_train,c)
+    performance = {'recall':recall,'precision':precision,'accuracy':accuracy,'auc':auc}
 
-y_proba = soed.predict_proba(X_test)
-y_pred = soed.predict(X_test)
+    print('test 1 passed.')
+    print(f'performance on a random dataset:{performance}')
 
-y_util = soed.predict_util(X_test)
-y_decide = soed.decide(X_test)
-
-
-
-print(recall_score(y_test,y_pred))
-print(precision_score(y_test,y_pred))
-print(accuracy_score(y_test,y_pred))
-print(roc_auc_score(y_test,y_proba[:,1]))
+except:
+    print("test 1 didn't pass.")
 
 
 
-print(soed)
+try:
+
+    print('starting test 2.')
+
+
+
+    # Load dataset
+    data = load_iris()
+    X = data.data
+    y = data.target  # Binary target
+
+    y = np.where(y==2,0,y)
+
+    X_standard = (X-X.min(axis=0))/(X.max(axis=0) - X.min(axis=0))
+
+    random_index = np.random.permutation(X.shape[0])
+    i = int(round(X.shape[0]*0.5))
+    train_index = random_index[:i]
+    test_index = random_index[i+1:]
+
+    X_train = X_standard[train_index]
+    X_test = X_standard[test_index]
+
+    y_train = y[train_index]
+    y_test = y[test_index]
+
+
+    c0 = np.where(y_train==0,0,20)
+    c1 = np.where(y_train==1,0,1)
+
+    c = np.column_stack((c0,c1))
+
+
+
+    soed = SOEDClassifier(mlp_max_iter=10000,som_x=10, som_y=10,som_input_len=X_train.shape[1])
+
+    soed.fit(X_train,y_train,c)
+
+    y_proba = soed.predict_proba(X_test)
+    y_pred = soed.predict(X_test)
+
+    y_util = soed.predict_util(X_test)
+    y_decide = soed.decide(X_test)
+
+
+
+    recall = recall_score(y_test,y_pred)
+    precision = precision_score(y_test,y_pred)
+    accuracy = accuracy_score(y_test,y_pred)
+    auc = roc_auc_score(y_test,y_proba[:,1])
+
+    performance = {'recall':recall,'precision':precision,'accuracy':accuracy,'auc':auc}
+
+    print('test 2 passed.')
+    print(f'performance on iris dataset with 20 to 1 cost:{performance}')
+
+except:
+    print("test 2 didn't pass.")
